5196de916d454b8f1725e4df81dc467635787fbe
iliastsangaris <iliastsangaris@gmail.com>
2014-04-16 14:53:25 -0400

    sticky nav bar

--- _assets/javascripts/app.js
+++ _assets/javascripts/app.js
@@ -11,32 +11,10 @@
 // HERO TEXT
 // Funcations run on page load
 $(document).ready(autoAdjustFontSize);
-$(document).ready(variableMargin);
 // Dynamically adjust textsize to page width
 function autoAdjustFontSize() {
   $(".title-text").fitText(1, {minFontSize: "55em", maxFontSize: "90em"});
-}
-
-// HERO ICONS
-// Call variableMargin() on window resize
-$(window).resize(variableMargin);
-// Make social icons have variable margin
-function variableMargin() {
-  var windowHeight = $(window).height();
-  var windowWidth = $(window).width();
-  var spacingDiv = Math.round((windowHeight/8)).toString();
-  var lineHeight = parseInt(spacingDiv * 1.2, 10).toString();
-  var fontSize = parseInt(spacingDiv / 1.2, 10).toString();
-  var emsToPx = $("#hero-title").height() / getEmSize("hero-title");
-  if (parseInt(lineHeight, 10) < 30) {
-    lineHeight = 20;
-  }
-  if (windowWidth < (460 * emsToPx)) {
-    spacingDiv = 10;
-  }
-  $(".social-icons").css("margin-top", spacingDiv + "px");
-  $(".social-icons a").css("line-height", lineHeight + "px");
-  $(".social-icons a").css("font-size", fontSize + "px");
+  $(".social-icons").fitText(1.2, {minFontSize: "40em", maxFontSize: "200em"});
 }
 
 // NAVBAR
@@ -46,7 +24,7 @@ $(window).scroll(stickyNav);
 function stickyNav(){
   var scrollDistance = $("body").scrollTop();
   var heroHeight = $(".hero-photo").height();
-  $(".sticky").toggleClass("sticking-nav", scrollDistance >= (heroHeight - 45));
+  $(".contain-to-grid").toggleClass("sticking-nav", scrollDistance >= (heroHeight - 45));
 }
 
 
